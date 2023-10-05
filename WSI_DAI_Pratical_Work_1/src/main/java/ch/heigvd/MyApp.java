81b9dff419317e0028764f9c839077f512742953
SlWa99 <walid.slimani@hes-so.ch>
2023-10-05 20:40:58 +0200

    Creation of a class file called MyApp. This class permits to add SubCommand. Now i can use the key word Replace and Convert to process my commands

--- WSI_DAI_Pratical_Work_1/src/main/java/ch/heigvd/MyApp.java
+++ WSI_DAI_Pratical_Work_1/src/main/java/ch/heigvd/MyApp.java
@@ -1,16 +1,14 @@
 /**
  *  -----------------------------------------------------------------------------------
- * @Fichier      : Main.java
+ * @Fichier      : MyApp.java
  * @Labo         : Laboratoire 1 : CLI using Picocli
  * @Authors      : Slimani Walid
- * @Date         : 28.09.2023
- *
+ * @Date         : 05.10.2023
  * @Description  : This program allows you to run line commands using Picocli. It features the following
  *                 functions:
  *                 - Replace a word : searches for a word in a text file and replaces either the first
  *                                    or all occurrences.
  *                 - Convert image to B&W : Converts an image to black and white.
- *
  * @Remarque     : /
  * @Modification : /
  *  -----------------------------------------------------------------------------------
@@ -18,15 +16,20 @@
 
 package ch.heigvd;
 
-import Commands.*;
+import Commands.ConvertBlackWhiteCommand;
+import Commands.ReplaceCommand;
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
-public class Main {
+@Command(name = "cli",
+         subcommands = {ReplaceCommand.class, ConvertBlackWhiteCommand.class, CommandLine.HelpCommand.class},
+         description = "My CLI app can be used to execute 2 commands. The first is called <Replace> and" +
+                       "the second is called <Convert>. if you need more information on how to use" +
+                       "the 2 commands, you can get an instruction by typing :" +
+                       "<Replace -help> or <Convert -help>.")
+public class MyApp {
     public static void main(String[] args) {
-        CommandLine commandLines = new CommandLine(new ReplaceCommand());
-        commandLines.addSubcommand("Convert", new CommandLine(new ConvertBlackWhiteCommand()));
-
-        int exitCode = commandLines.execute(args);
+        int exitCode = new CommandLine(new MyApp()).execute(args);
         System.exit(exitCode);
     }
 }
